f8327b77eba26ae569cea4c0eac65c218d348556
bibinba <bibi@gma.com>
2020-02-26 12:05:29 +0900

    スクリプト追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Sprite Sprite_Gu;
+    [SerializeField] private Sprite Sprite_Tyoki;
+    [SerializeField] private Sprite Sprite_Pa;
+    [SerializeField] private Image Image_Hand;
+    [SerializeField] private Button Button_Gu;
+
     void Start()
     {
-        
+        SetRandomHandSprite();
+        //Button_Gu.onClick.AsObservable().Subscribe((_) => isGameStart = true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+
+    void SetRandomHandSprite()
+    {
+        int randint = Random.Range(1, 4);
+        Debug.Log("randint:"+randint);
+
+        if (randint == 1)
+        {            
+            Image_Hand.sprite = Sprite_Gu;
+        }
+        else if (randint == 2)
+        {
+            Image_Hand.sprite = Sprite_Tyoki;
+        }
+        else
+        {
+            Image_Hand.sprite = Sprite_Pa;
+        }
     }
 }
